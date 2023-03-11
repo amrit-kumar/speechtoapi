bb81f376c49f455926761f7baf4214fd893db0c9
Amrit Kumar <kumaramrit38@gmail.com>
2023-03-11 20:17:31 +0530

    first commit

---
+++ src/main/java/com/banking/speechapi/SpeechapiApplication.java
@@ -0,0 +1,13 @@
+package com.banking.speechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechapiApplication.class, args);
+	}
+
+}
